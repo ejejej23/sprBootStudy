8417bba6d76a7e4bfec2c2d023e5d290b57f4b54
yannishin <yannishin@gmail.com>
2022-01-06 09:31:08 +0900

    java 8 로 변경

--- src/main/java/com/naon/study/property/TimezoneProperties.java
+++ src/main/java/com/naon/study/property/TimezoneProperties.java
@@ -1,8 +1,18 @@
 package com.naon.study.property;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author yannishin
  */
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix="app-config.timezone")
 public class TimezoneProperties {
-
+    private String defaultTimezone;
+    private String convertTimezone;
 }
