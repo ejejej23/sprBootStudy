2999d0265edf56f8f45c8029f09dafb30fe3c8a6
= <=>
2022-01-08 13:18:22 +0900

    최초 커밋

---
+++ src/main/java/com/example/sprbootstudy/SprBootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprBootStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprbootstudy/SprBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
